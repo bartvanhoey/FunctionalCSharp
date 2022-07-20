9dc3a9d55e2d87720ba98ab85d4cc692eba94190
bartvanhoey <bartvanhoey@hotmail.com>
2022-07-20 18:51:38 +0200

    Aggregate method examples

---
+++ FunctionalCSharp.Tests/Aggregate/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace FunctionalCSharp.Tests.Aggregate
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Salary { get; set; }
+        public string? Department { get; set; }
+
+        public static List<Employee> GetAllEmployees()
+        {
+            return new List<Employee>
+            {
+                new() { Id = 101, Name = "Preety", Salary = 10000, Department = "IT" },
+                new() { Id = 102, Name = "Priyanka", Salary = 15000, Department = "Sales" },
+                new() { Id = 103, Name = "James", Salary = 50000, Department = "Sales" },
+                new() { Id = 104, Name = "Hina", Salary = 20000, Department = "IT" },
+                new() { Id = 105, Name = "Anurag", Salary = 30000, Department = "IT" },
+            };
+        }
+    }
+}
