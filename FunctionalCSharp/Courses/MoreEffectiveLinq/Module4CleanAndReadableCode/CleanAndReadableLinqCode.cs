1a10f4d31ba581c3118a19547d6dfe5b25c357e5
bartvanhoey <bartvanhoey@hotmail.com>
2022-07-27 08:27:44 +0200

    Sort by age

---
+++ FunctionalCSharp/Courses/MoreEffectiveLinq/Module4CleanAndReadableCode/CleanAndReadableLinqCode.cs
@@ -0,0 +1,26 @@
+﻿using static System.DateTime;
+using static System.Globalization.CultureInfo;
+
+namespace FunctionalCSharp.Courses.MoreEffectiveLinq.Module4CleanAndReadableCode
+{
+    public static class CleanAndReadableLinqCode
+    {
+        public static IEnumerable<PlayerAge> GetPlayerAge(string players) =>
+            players
+                .Split(';')
+                .Select(n => n.Split(','))
+                .Select(x => new
+                {
+                    Name = x[0].Trim(),
+                    DateOfBirth = ParsDob(x[1])
+                })
+                .OrderByDescending(n => n.DateOfBirth)
+                .Select(n =>
+                {
+                    var age = n.DateOfBirth.CalculateAge();
+                    return new PlayerAge(n.Name, age);
+                });
+
+        private static DateTime ParsDob(string dob) => ParseExact(dob.Trim(), "d/M/yyyy", InvariantCulture);
+    }
+}
---
+++ FunctionalCSharp/Courses/MoreEffectiveLinq/Module4CleanAndReadableCode/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace FunctionalCSharp.Courses.MoreEffectiveLinq.Module4CleanAndReadableCode
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+            if (dateOfBirth.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
