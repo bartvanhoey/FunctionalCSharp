ac398894b0034522e98d66103949524b82663825
ekapituro <ekapituro@gmail.com>
2024-05-15 20:40:18 +0200

    Update

--- FunctionalCSharp/Books/FunctionalProgrammingInCSharp/Part2_CoreTechniques/Chap7_FunctionComposition/BankOfCodeland/BicFormatValidatorC7.cs
+++ FunctionalCSharp/Books/FunctionalProgrammingInCSharp/Part2_CoreTechniques/Chap7_FunctionComposition/BankOfCodeland/BicFormatValidatorC7.cs
@@ -6,5 +6,6 @@ public sealed class BicFormatValidatorC7 : IValidatorC7<MakeTransferC7>
 {
     static readonly Regex regex = new Regex("^[A-Z]{6}[A-Z1-9]{5}$");
 
+    // comment
     public bool IsValid(MakeTransferC7 t) => regex.IsMatch(t.Bic);
 }
